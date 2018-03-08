68fc4aa99e426ae5b0ad9326870387c839dc44a7
Ferran Basora <fcsonline@gmail.com>
2018-03-08 23:25:06 +0100

    Relative path to benchmark file

--- src/expandable/include.rs
+++ src/expandable/include.rs
@@ -1,5 +1,6 @@
 use std::process;
 use yaml_rust::{YamlLoader, Yaml};
+use std::path::Path;
 
 use expandable::{multi_request, multi_csv_request, include};
 use actions;
@@ -11,14 +12,16 @@ pub fn is_that_you(item: &Yaml) -> bool{
   item["include"].as_str().is_some()
 }
 
-pub fn expand(item: &Yaml, mut list: &mut Vec<Box<(Runnable + Sync + Send)>>) {
-  let path = item["include"].as_str().unwrap();
+pub fn expand(parent_path: &str, item: &Yaml, mut list: &mut Vec<Box<(Runnable + Sync + Send)>>) {
+  let include_path = item["include"].as_str().unwrap();
+  let include_filepath = Path::new(parent_path).with_file_name(include_path);
+  let final_path = include_filepath.to_str().unwrap();
 
-  expand_from_filepath(path, &mut list, None);
+  expand_from_filepath(final_path, &mut list, None);
 }
 
-pub fn expand_from_filepath(path: &str, mut list: &mut Vec<Box<(Runnable + Sync + Send)>>, accessor: Option<&str>) {
-  let benchmark_file = reader::read_file(path);
+pub fn expand_from_filepath(parent_path: &str, mut list: &mut Vec<Box<(Runnable + Sync + Send)>>, accessor: Option<&str>) {
+  let benchmark_file = reader::read_file(parent_path);
 
   let docs = YamlLoader::load_from_str(benchmark_file.as_str()).unwrap();
   let doc = &docs[0];
@@ -41,9 +44,9 @@ pub fn expand_from_filepath(path: &str, mut list: &mut Vec<Box<(Runnable + Sync 
     if multi_request::is_that_you(item) {
       multi_request::expand(item, &mut list);
     } else if multi_csv_request::is_that_you(item) {
-      multi_csv_request::expand(item, &mut list);
+      multi_csv_request::expand(parent_path, item, &mut list);
     } else if include::is_that_you(item) {
-      include::expand(item, &mut list);
+      include::expand(parent_path, item, &mut list);
     } else if actions::Assign::is_that_you(item) {
       list.push(Box::new(actions::Assign::new(item, None)));
     } else if actions::Request::is_that_you(item){
--- src/expandable/multi_csv_request.rs
+++ src/expandable/multi_csv_request.rs
@@ -1,4 +1,5 @@
 use yaml_rust::Yaml;
+use std::path::Path;
 
 use actions::{Request, Runnable};
 use reader;
@@ -8,12 +9,15 @@ pub fn is_that_you(item: &Yaml) -> bool{
     item["with_items_from_csv"].as_str().is_some()
 }
 
-pub fn expand(item: &Yaml, list: &mut Vec<Box<(Runnable + Sync + Send)>>) {
+pub fn expand(parent_path: &str, item: &Yaml, list: &mut Vec<Box<(Runnable + Sync + Send)>>) {
   let with_items_from_csv_option = item["with_items_from_csv"].as_str();
 
   if with_items_from_csv_option.is_some() {
     let with_items_path = with_items_from_csv_option.unwrap();
-    let with_items_file = reader::read_csv_file_as_yml(with_items_path);
+    let with_items_filepath = Path::new(parent_path).with_file_name(with_items_path);
+    let final_path = with_items_filepath.to_str().unwrap();
+
+    let with_items_file = reader::read_csv_file_as_yml(final_path);
 
     for with_item in with_items_file {
       list.push(Box::new(Request::new(item, Some(with_item))));
