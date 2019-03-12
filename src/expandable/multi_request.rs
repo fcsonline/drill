8d8bd88f122170a5667fb049de66d89b56bcdc01
Ferran Basora <fcsonline@gmail.com>
2019-03-12 23:49:37 +0000

    Add some tests

--- src/expandable/multi_request.rs
+++ src/expandable/multi_request.rs
@@ -18,3 +18,22 @@ pub fn expand(item: &Yaml, list: &mut Vec<Box<(Runnable + Sync + Send)>>) {
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use actions::Runnable;
+
+  #[test]
+  fn expand_multi () {
+    let text = "---\nname: foobar\nrequest:\n  url: /api/{{ item }}\nwith_items:\n  - 1\n  - 2\n  - 3";
+    let docs = yaml_rust::YamlLoader::load_from_str(text).unwrap();
+    let doc = &docs[0];
+    let mut list: Vec<Box<(Runnable + Sync + Send)>> = Vec::new();
+
+    expand(&doc, &mut list);
+
+    assert_eq!(is_that_you(&doc), true);
+    assert_eq!(list.len(), 3);
+  }
+}
