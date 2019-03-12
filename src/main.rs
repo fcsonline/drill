682409df760880b6b3bc984c27937392d4e19648
Joern Ploennigs <joern.ploennigs@ie.ibm.com>
2019-03-12 18:20:07 +0000

    add report per job name

--- src/main.rs
+++ src/main.rs
@@ -90,73 +90,86 @@ fn app_args<'a> () -> clap::ArgMatches<'a> {
     .get_matches();
 }
 
-fn compute_stats (list_reports: &Vec<Report>) -> (usize, usize, usize, f64) {
+struct DrillStats {
+  total_requests: usize,
+  successful_requests: usize,
+  failed_requests: usize,
+  mean_duration: f64,
+  median_duration: f64,
+  stdev_duration: f64,
+}
+
+fn compute_stats (sub_reports: &Vec<Report>) -> DrillStats {
   let mut group_by_status = HashMap::new();
 
-  for req in list_reports.concat() {
+  for req in sub_reports {
     group_by_status.entry(req.status / 100).or_insert(Vec::new()).push(req);
   }
 
-  let durations = list_reports.concat().iter().map(|r| r.duration).collect::<Vec<f64>>();
-  let mean = durations.iter().fold(0f64, |a, &b| a + b) / durations.len() as f64;
-  let deviations = durations.iter().map(|a| (mean - a).powf(2.0)).collect::<Vec<f64>>();
-  let stdev = (deviations.iter().fold(0f64, |a, &b| a + b) / durations.len() as f64).sqrt();
+  let durations = sub_reports.iter().map(|r| r.duration).collect::<Vec<f64>>();
+  let mean_duration = durations.iter().fold(0f64, |a, &b| a + b) / durations.len() as f64;
+  let deviations = durations.iter().map(|a| (mean_duration - a).powf(2.0)).collect::<Vec<f64>>();
+  let stdev_duration = (deviations.iter().fold(0f64, |a, &b| a + b) / durations.len() as f64).sqrt();
 
   let mut sorted = durations.clone();
   sorted.sort_by(|a, b| a.partial_cmp(b).unwrap());
   let durlen = sorted.len();
-  let median = if durlen % 2 == 0 {
+  let median_duration = if durlen % 2 == 0 {
     sorted[durlen / 2]
   } else {
     (sorted[durlen / 2] + sorted[durlen / 2 + 1]) / 2f64
   };
 
-  let total_requests = list_reports.concat().len();
+  let total_requests = sub_reports.len();
   let successful_requests = group_by_status.entry(2).or_insert(Vec::new()).len();
   let failed_requests = total_requests - successful_requests;
-  let requests_per_second = total_requests as f64 / duration;
 
-  (total_requests, successful_requests, failed_requests, requests_per_second)
+  DrillStats{
+    total_requests, 
+    successful_requests, 
+    failed_requests,
+    mean_duration,
+    median_duration,
+    stdev_duration
+  }
 }
 
 fn show_stats (list_reports: &Vec<Vec<Report>>, stats_option: bool, duration: f64) {
   if !stats_option { return }
 
-  let mut group_by_status = HashMap::new();
+  let mut group_by_name = HashMap::new();
 
   for req in list_reports.concat() {
-    group_by_status.entry(req.status / 100).or_insert(Vec::new()).push(req);
+    group_by_name.entry(req.name.clone()).or_insert(Vec::new()).push(req);
   }
 
-  let durations = list_reports.concat().iter().map(|r| r.duration).collect::<Vec<f64>>();
-  let mean = durations.iter().fold(0f64, |a, &b| a + b) / durations.len() as f64;
-  let deviations = durations.iter().map(|a| (mean - a).powf(2.0)).collect::<Vec<f64>>();
-  let stdev = (deviations.iter().fold(0f64, |a, &b| a + b) / durations.len() as f64).sqrt();
+  // compute stats per name
+  for (name, reports) in group_by_name {
+    let substats = compute_stats(&reports);
+    println!("");
+    println!("{} {} {}", name.green(), "Total requests".yellow(), substats.total_requests.to_string().purple());
+    println!("{} {} {}", name.green(), "Successful requests".yellow(), substats.successful_requests.to_string().purple());
+    println!("{} {} {}", name.green(), "Failed requests".yellow(), substats.failed_requests.to_string().purple());
+    println!("{} {} {}{}", name.green(), "Median time per request".yellow(), substats.median_duration.round().to_string().purple(), "ms".purple());
+    println!("{} {} {}{}", name.green(), "Average time per request".yellow(), substats.mean_duration.round().to_string().purple(), "ms".purple());
+    println!("{} {} {}{}", name.green(), "Sample standard deviation".yellow(), substats.stdev_duration.round().to_string().purple(), "ms".purple());
+  }
 
-  let mut sorted = durations.clone();
-  sorted.sort_by(|a, b| a.partial_cmp(b).unwrap());
-  let durlen = sorted.len();
-  let median = if durlen % 2 == 0 {
-    sorted[durlen / 2]
-  } else {
-    (sorted[durlen / 2] + sorted[durlen / 2 + 1]) / 2f64
-  };
-
-  let total_requests = list_reports.concat().len();
-  let successful_requests = group_by_status.entry(2).or_insert(Vec::new()).len();
-  let failed_requests = total_requests - successful_requests;
-  let requests_per_second = total_requests as f64 / duration;
+  // compute global stats
+  let allreports = list_reports.concat();
+  let global_stats = compute_stats(&allreports);
+  let requests_per_second = global_stats.total_requests as f64 / duration;
 
   println!("");
   println!("{} {}", "Concurrency Level".yellow(), list_reports.len().to_string().purple());
   println!("{} {} {}", "Time taken for tests".yellow(), format!("{:.1}", duration).to_string().purple(), "seconds".purple());
-  println!("{} {}", "Total requests".yellow(), total_requests.to_string().purple());
-  println!("{} {}", "Successful requests".yellow(), successful_requests.to_string().purple());
-  println!("{} {}", "Failed requests".yellow(), failed_requests.to_string().purple());
+  println!("{} {}", "Total requests".yellow(), global_stats.total_requests.to_string().purple());
+  println!("{} {}", "Successful requests".yellow(), global_stats.successful_requests.to_string().purple());
+  println!("{} {}", "Failed requests".yellow(), global_stats.failed_requests.to_string().purple());
+  println!("{} {}{}", "Median time per request".yellow(), global_stats.median_duration.round().to_string().purple(), "ms".purple());
+  println!("{} {}{}", "Average time per request".yellow(), global_stats.mean_duration.round().to_string().purple(), "ms".purple());
+  println!("{} {}{}", "Sample standard deviation".yellow(), global_stats.stdev_duration.round().to_string().purple(), "ms".purple());
   println!("{} {} {}", "Requests per second".yellow(), format!("{:.2}", requests_per_second).to_string().purple(), "[#/sec]".purple());
-  println!("{} {}{}", "Median time per request".yellow(), median.round().to_string().purple(), "ms".purple());
-  println!("{} {}{}", "Average time per request".yellow(), mean.round().to_string().purple(), "ms".purple());
-  println!("{} {}{}", "Sample standard deviation".yellow(), stdev.round().to_string().purple(), "ms".purple());
 }
 
 fn compare_benchmark (list_reports: &Vec<Vec<Report>>, compare_path_option: Option<&str>, threshold_option: Option<&str>) {
