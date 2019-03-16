35351b109005d650f388d45fcd1d216394fbfe84
Barry Roberts <manithree@gmail.com>
2019-03-16 09:57:29 -0600

    Use if let for more idiomatic rust

--- src/expandable/multi_csv_request.rs
+++ src/expandable/multi_csv_request.rs
@@ -11,32 +11,24 @@ pub fn is_that_you(item: &Yaml) -> bool{
 }
 
 pub fn expand(parent_path: &str, item: &Yaml, list: &mut Vec<Box<(Runnable + Sync + Send)>>) {
-  let with_items_from_csv_option = item["with_items_from_csv"].as_str();
-  let mut quote_char : u8 = '\"' as u8;
-  let with_items_path : &str;
+    let (with_items_path, quote_char) =
+        if let Some(with_items_path) = item["with_items_from_csv"].as_str() {
+            (with_items_path, '\"' as u8)
+        } else if let Some(_with_items_hash) = item["with_items_from_csv"].as_hash() {
+            let with_items_path = item["with_items_from_csv"]["file_name"].as_str().expect("Expected a file_name");
+            let quote_char = item["with_items_from_csv"]["quote_char"].as_str().unwrap_or("\"").bytes().nth(0).unwrap();
 
-  if with_items_from_csv_option.is_some() {
-    // handle the single string case
-    with_items_path = with_items_from_csv_option.unwrap();
-  }
-  else if item["with_items_from_csv"].as_hash().is_some() {
-    // handle the hash (file_name and quote_char) case
-    with_items_path = item["with_items_from_csv"]["file_name"].as_str().unwrap();
+            (with_items_path, quote_char)
+        } else {
+            panic!("WAT"); // Impossible case
+        };
 
-    if item["with_items_from_csv"]["quote_char"].as_str().is_some() {
-      quote_char = item["with_items_from_csv"]["quote_char"].as_str().unwrap().bytes().nth(0).unwrap();
+    let with_items_filepath = Path::new(parent_path).with_file_name(with_items_path);
+    let final_path = with_items_filepath.to_str().unwrap();
+
+    let with_items_file = reader::read_csv_file_as_yml(final_path, quote_char);
+
+    for with_item in with_items_file {
+        list.push(Box::new(Request::new(item, Some(with_item))));
     }
-
-  }
-  else { 
-    return
-  }
-  let with_items_filepath = Path::new(parent_path).with_file_name(with_items_path);
-  let final_path = with_items_filepath.to_str().unwrap();
-
-  let with_items_file = reader::read_csv_file_as_yml(final_path, quote_char);
-
-  for with_item in with_items_file {
-    list.push(Box::new(Request::new(item, Some(with_item))));
-  }
 }
