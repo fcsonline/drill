f3be0a6337e66a1c656c4dc488869f6d02c25864
Ferran Basora <fcsonline@gmail.com>
2020-05-23 16:26:40 +0000

    Use more type alias

--- src/expandable/multi_request.rs
+++ src/expandable/multi_request.rs
@@ -1,15 +1,16 @@
 use yaml_rust::Yaml;
 
-use crate::actions::{Request, Runnable};
+use crate::benchmark::Benchmark;
+use crate::actions::Request;
 
 pub fn is_that_you(item: &Yaml) -> bool {
   item["request"].as_hash().is_some() && item["with_items"].as_vec().is_some()
 }
 
-pub fn expand(item: &Yaml, list: &mut Vec<Box<(dyn Runnable + Sync + Send)>>) {
+pub fn expand(item: &Yaml, benchmark: &mut Benchmark) {
   if let Some(with_items) = item["with_items"].as_vec() {
     for with_item in with_items {
-      list.push(Box::new(Request::new(item, Some(with_item.clone()))));
+      benchmark.push(Box::new(Request::new(item, Some(with_item.clone()))));
     }
   }
 }
@@ -17,18 +18,17 @@ pub fn expand(item: &Yaml, list: &mut Vec<Box<(dyn Runnable + Sync + Send)>>) {
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::actions::Runnable;
 
   #[test]
   fn expand_multi() {
     let text = "---\nname: foobar\nrequest:\n  url: /api/{{ item }}\nwith_items:\n  - 1\n  - 2\n  - 3";
     let docs = yaml_rust::YamlLoader::load_from_str(text).unwrap();
     let doc = &docs[0];
-    let mut list: Vec<Box<(dyn Runnable + Sync + Send)>> = Vec::new();
+    let mut benchmark: Benchmark = Benchmark::new();
 
-    expand(&doc, &mut list);
+    expand(&doc, &mut benchmark);
 
     assert_eq!(is_that_you(&doc), true);
-    assert_eq!(list.len(), 3);
+    assert_eq!(benchmark.len(), 3);
   }
 }
