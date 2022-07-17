1dcc3db6490fdd279d736786745e296786b21633
Hendric Eckelt <Hendric.Eckelt@gmail.com>
2022-07-17 13:22:47 +0000

    Implement a pick option
    Allows to pick a subset of items from with_items.
    Pick limits the amount of requests done instead of doing one for each item.
    When used in conjunction with shuffle it can be used to send one request with a random item from the list.
    
    Resolves #131

--- src/expandable/mod.rs
+++ src/expandable/mod.rs
@@ -4,3 +4,63 @@ mod multi_csv_request;
 mod multi_file_request;
 mod multi_iter_request;
 mod multi_request;
+
+use yaml_rust::Yaml;
+
+pub fn pick(item: &Yaml, with_items: &Vec<Yaml>) -> usize {
+  match item["pick"].as_i64() {
+    Some(value) => {
+      if value.is_negative() {
+        panic!("pick option should not be negative, but was {}", value);
+      } else if value as usize > with_items.len() {
+        panic!("pick option should not be greater than the provided items, but was {}", value);
+      } else {
+        value as usize
+      }
+    }
+    None => with_items.len()
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  mod pick {
+    use super::*;
+
+    #[test]
+    fn should_return_the_configured_value() {
+      let text = "---\nname: foobar\nrequest:\n  url: /api/{{ item }}\npick: 2\nwith_items:\n  - 1\n  - 2\n  - 3";
+      let item = &yaml_rust::YamlLoader::load_from_str(text).unwrap()[0];
+      let pick = pick(item, item["with_items"].as_vec().unwrap());
+
+      assert_eq!(pick, 2);
+    }
+
+    #[test]
+    fn should_return_the_with_items_length_if_unset() {
+      let text = "---\nname: foobar\nrequest:\n  url: /api/{{ item }}\nwith_items:\n  - 1\n  - 2\n  - 3";
+      let item = &yaml_rust::YamlLoader::load_from_str(text).unwrap()[0];
+      let pick = pick(item, item["with_items"].as_vec().unwrap());
+
+      assert_eq!(pick, 3);
+    }
+
+    #[test]
+    #[should_panic(expected = "pick option should not be negative, but was -1")]
+    fn should_panic_for_negative_values() {
+      let text = "---\nname: foobar\nrequest:\n  url: /api/{{ item }}\npick: -1\nwith_items:\n  - 1\n  - 2\n  - 3";
+      let item = &yaml_rust::YamlLoader::load_from_str(text).unwrap()[0];
+      pick(item, item["with_items"].as_vec().unwrap());
+    }
+
+    #[test]
+    #[should_panic(expected = "pick option should not be greater than the provided items, but was 4")]
+    fn should_panic_for_values_greater_than_the_items_list() {
+      let text = "---\nname: foobar\nrequest:\n  url: /api/{{ item }}\npick: 4\nwith_items:\n  - 1\n  - 2\n  - 3";
+      let item = &yaml_rust::YamlLoader::load_from_str(text).unwrap()[0];
+      pick(item, item["with_items"].as_vec().unwrap());
+    }
+  }
+}
--- src/expandable/multi_file_request.rs
+++ src/expandable/multi_file_request.rs
@@ -1,12 +1,12 @@
+use crate::actions::Request;
 use crate::benchmark::Benchmark;
 use crate::interpolator::INTERPOLATION_REGEX;
+use crate::reader;
+use super::pick;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::path::Path;
 use yaml_rust::Yaml;
-
-use crate::actions::Request;
-use crate::reader;
 
 pub fn is_that_you(item: &Yaml) -> bool {
   item["request"].as_hash().is_some() && (item["with_items_from_file"].as_str().is_some() || item["with_items_from_file"].as_hash().is_some())
@@ -35,7 +35,8 @@ pub fn expand(parent_path: &str, item: &Yaml, benchmark: &mut Benchmark) {
     }
   }
 
-  for (index, with_item) in with_items_file.iter().enumerate() {
+  let pick = pick(item, &with_items_file);
+  for (index, with_item) in with_items_file.iter().take(pick).enumerate() {
     let index = index as u32;
 
     benchmark.push(Box::new(Request::new(item, Some(with_item.clone()), Some(index))));
@@ -57,5 +58,31 @@ mod test {
 
     assert_eq!(is_that_you(doc), true);
     assert_eq!(benchmark.len(), 3);
+  }
+
+  #[test]
+  fn expand_multi_should_limit_requests_using_the_pick_option() {
+    let text = "---\nname: foobar\nrequest:\n  url: /api/{{ item }}\npick: 2\nwith_items_from_file: ./fixtures/texts.txt";
+    let docs = yaml_rust::YamlLoader::load_from_str(text).unwrap();
+    let doc = &docs[0];
+    let mut benchmark: Benchmark = Benchmark::new();
+
+    expand("example/benchmark.yml", doc, &mut benchmark);
+
+    assert_eq!(is_that_you(doc), true);
+    assert_eq!(benchmark.len(), 2);
+  }
+
+  #[test]
+  fn expand_multi_should_work_with_pick_and_shuffle() {
+    let text = "---\nname: foobar\nrequest:\n  url: /api/{{ item }}\npick: 1\nshuffle: true\nwith_items_from_file: ./fixtures/texts.txt";
+    let docs = yaml_rust::YamlLoader::load_from_str(text).unwrap();
+    let doc = &docs[0];
+    let mut benchmark: Benchmark = Benchmark::new();
+
+    expand("example/benchmark.yml", doc, &mut benchmark);
+
+    assert_eq!(is_that_you(doc), true);
+    assert_eq!(benchmark.len(), 1);
   }
 }
